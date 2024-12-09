b805b2b17c610c82b1ac969431ac6773d4d0312f
faruq2991 <faruqadam.92@gmail.com>
2024-12-09 12:00:35 +0100

    ...

---
+++ day01/day01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "Creating a new Directory...."
+
+mkdir my_project && cd my_project && touch file1.txt file2.txt file3.txt
+
+
+echo "Project setup complete!."
+
+
+ls my_project
